0d558f761b26cf2e38661364c5534f083369474c
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-07-21 16:54:40 +0530

    fixed sticky nav

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-// var waypoints = $('#handler-first').waypoint(function(direction) {
-//     notify(this.element.id + ' hit 25% from top of window')
-//   }, {
-//     offset: '25%'
-//   })
-
 $(document).ready(function () {
   $(".feature").waypoint(function (direction) {
     if (direction == "down") {
       $("nav").addClass("sticky");
+      $(".nav-container").css("padding", "1.5rem 0");
     } else {
       $("nav").removeClass("sticky");
+      $(".nav-container").css("padding", "3rem 0");
     }
   });
 });
