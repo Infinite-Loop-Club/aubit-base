92e36f16a730eb64a69b035bb2a56c630dbb6d34
Pranav <pranav.gnanasekar23@gmail.com>
2020-07-22 13:13:38 +0530

    sidebar problem

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
-    $(".feature").waypoint(function (direction) {
-        if (direction == "down") {
-            $("nav").addClass("sticky");
-            $(".nav-container").css("padding", "1.5rem 0");
-        } else {
-            $("nav").removeClass("sticky");
-            $(".nav-container").css("padding", "3rem 0");
-        }
-    }, {
-        offset: '50%'
-    });
+  $(".feature").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+        $(".nav-container").css("padding", "1.5rem 0");
+      } else {
+        $("nav").removeClass("sticky");
+        $(".nav-container").css("padding", "3rem 0");
+      }
+    },
+    {
+      offset: "50%",
+    }
+  );
 });
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__list");
+
+  burger.addEventListener("click", () => {
+    console.log("clicked");
+    // nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
